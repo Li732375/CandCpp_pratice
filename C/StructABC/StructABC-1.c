f65b0058552113ea25595e2dc21a7432cf3632fa
Li732375 <lee732375@gmail.com>
2024-01-07 11:34:44 +0800

    learning updated

---
+++ C/StructABC/StructABC-1.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+
+// 使用 typedef，定義一個比較容易閱讀的名稱。
+typedef const char* String; // 定義結構的別名。
+
+// 注意這裡
+// 使用 typedef 定義結構的別名，如此一來，
+// 宣告並產生實例時，就不用再寫 struct 關
+// 鍵字。
+typedef struct Account { 
+    String id;
+    String name;
+    double balance;
+} Account; // 留意這裡也要補齊名稱。
+
+// 以下的範例定義的結構型態為 struct <anonymous>，
+// 也就是匿名結構，並以 typedef 取這個匿名結構取了
+// 個別名。
+typedef struct {// 這裡的 Account 不見了 
+    String id;
+    String name;
+    double balance;
+} Account;
+
+// 結構的名稱型態"無需"包含 struct 名稱。
+void printAcct(Account acct) {
+    printf("Account(%s, %s, %.2f)\n", acct.id, acct.name, acct.balance);
+}
+
+
+int main(void){
+    // 結構的名稱型態"無需"包含 struct 名稱。
+    Account acct; // 建立 Account 的實例。
+
+    acct.id = "123-456-789";
+    acct.name = "Justin Lin";
+    acct.balance = 1000;
+
+    // 結構的名稱型態"無需"包含 struct 名稱。
+    Account acct_1 = {"456-123-789", "Tin Lin", 100};
+
+    // 結構的名稱型態"無需"包含 struct 名稱。
+    Account acct_2 = {.balance = 10, .id = "789-123-456",
+     .name = "Tom Lee"};
+
+    // 結構的名稱型態"無需"包含 struct 名稱。
+    Account accts_3 [] = {
+        {"123-456-789", "Justin Lin", 1000}, 
+        {"789-654-321", "Monica Huang", 1000} 
+    };
+
+    printAcct(acct);
+    printAcct(acct_1);
+    printAcct(acct_2);
+
+    for(int i = 0; i < 2; i++){
+        printAcct(accts_3[i]);
+    }
+
+
+    return 0;
+}
---
+++ C/StructABC/StructABC.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+
+// 使用 typedef，定義一個比較容易閱讀的名稱。
+typedef const char* String; // 定義結構的別名，後面範例會提及。
+
+// 有些資料會有相關性，相關聯的資料組織在一起，
+// 對於資料本身的可用性或者是程式碼的可讀性，都
+// 會有所幫助。
+struct Account { 
+    // 值域
+    String id; // 結構別名
+    String name; // 結構別名
+    double balance;
+};
+// 也可以在函式中定義結構，直接宣告結構實例。
+//struct Account { 
+//    String id;
+//    String name;
+//    double balance;
+//} acct_1 = {"789-456-123", "Lin", 101};
+
+void printAcct(struct Account acct) {
+    printf("Account(%s, %s, %.2f)\n", acct.id, acct.name, acct.balance);
+}
+
+
+int main(void){
+    // 結構的名稱型態包含 struct 這個名稱。
+    struct Account acct; // struct Account acct 建立了 Account 的實例。
+
+    // 存取實例的值域時可以使用 dot 運算子「.」。
+    acct.id = "123-456-789";
+    acct.name = "Justin Lin";
+    acct.balance = 1000;
+
+    // 結果會不可預期，如果沒有指定值域的初值也是如此。
+    // 可以如下指定每個值域的初值。
+    struct Account acct_1 = {"456-123-789", "Tin Lin", 100};
+
+    // 也可以使用成員名稱，不一定要按照成員名稱的定義順序來指定。
+    struct Account acct_2 = {.balance = 10, .id = "789-123-456",
+     .name = "Tom Lee"};
+
+    // 若要宣告結構陣列並初始每個結構成員。
+    struct Account accts_3 [] = {
+        {"163-285-749", "Jhon", 170}, 
+        {"789-654-321", "Monica Huang", 105} 
+    };
+
+    // 顯示 Account(123-456-789, Justin Lin, 1000.000000)
+    printAcct(acct);
+    // acct 在函式執行完畢後就會自動清除。
+    printAcct(acct_1);
+    printAcct(acct_2);
+
+    // 顯示結構陣列
+    for(int i = 0; i < 2; i++){
+        printAcct(accts_3[i]);
+    }
+
+    return 0;
+}
