e4f4e09c08ce5d94fb8d06c0e6abe22eb45c891e
Li732375 <lee732375@gmail.com>
2024-01-10 10:48:23 +0800

    updated

--- C/hello_world/hello_world.c
+++ C/hello_world/hello_world.c
@@ -1,6 +1,12 @@
 #include <stdio.h> // 巨集（Macro）前置處理器指令（Preprocessor directive）
 // 告訴編譯器（Compiler）這個程式會用到 stdio.h 這個標頭檔（Header file）
-// 定義的函式（Function），以便正確地編譯程式中使用到的程式庫函式
+// 定義的函式（Function），以便正確地編譯程式中使用到的程式庫函式。
+
+// 表示將含括的檔案插入目前原始碼之中。
+
+// 在 terminal 使用 gcc 的話，可以指定 -E 表示只進行前置處理
+// gcc -E hello_world.c -o hello_world.i
+// 開啟 main.i 的話，你會發現在 main 函式定義之前，安插了 stdio.h 的內容。 
 
 int main() // 程式的進入點（Entry point）
 {
