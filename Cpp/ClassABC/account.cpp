5bed5a3bf17f1ecfd8832def6a519790c76a569d
Li732375 <lee732375@gmail.com>
2024-01-24 11:18:14 +0800

    learning updated

---
+++ Cpp/ClassABC/account.cpp
@@ -0,0 +1,38 @@
+#include <iostream> 
+#include <string>
+#include "account.h"
+using namespace std;
+
+// :: 是類別範圍解析（class scope resolution）運算子，
+// 在實作類別建構式或方法（method）時，在 ::前指明實作
+// 哪類別之定義。
+Account::Account(string id, string name, double balance) {
+    // 在建構式（constructor）或方法的實作中，若要存取實例本身，
+    // 可以透過 this，這是個指標，因此要透過箭號運算子來存取值域。
+    this->id = id;
+    this->name = name;
+    this->balance = balance;
+}
+
+string Account::to_string() {
+    return string("Account(") + 
+           this->id + ", " +
+           this->name + ", " +
+           std::to_string(this->balance) + ")"; // 
+}
+
+void Account::deposit(double amount) {
+    if(amount <= 0) {
+        cout << "必須存入正數" << endl;
+        return;
+    }
+    this->balance += amount;
+}
+
+void Account::withdraw(double amount) {
+    if(amount > this->balance) {
+        cout << "餘額不足" << endl;
+        return;
+    }
+    this->balance -= amount;
+}
---
+++ Cpp/ClassABC/account.h
@@ -0,0 +1,26 @@
+#include <string>
+using namespace std; 
+
+// 從 C 背景來的開發者可能會想，這種風格像是 C 的結構（struct），
+// 在 C++ 中，struct 也被視為定義類別，將以上的 class 關鍵字換為 
+// struct，程式也可以運作，struct 與 class 的差別在於，前者在第一
+// 個權限可見的修飾詞出現前（例如 public、private），定義的成員預
+// 設會是公開可存取，而後者預設會是私有（也就是 private）。
+class Account { 
+private: 
+    // 值域（field）
+    string id;  
+    string name; 
+    double balance;
+
+public: 
+    // 值域（field）
+
+    // 如果沒有定義任何建構式，編譯器會自動產生沒有參數的預設建構式，
+    // 如果自定義了建構式，就是類別內初始式（in-class initializer），
+    // 就會使用你定義的建構式。
+    Account(string id, string name, double balance); // 建構式，初始 Account 值域。
+    void deposit(double amount);
+    void withdraw(double amount);
+    string to_string();
+};
