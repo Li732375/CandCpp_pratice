2bebf9f158ce96e6f33d5885e7d3615ac578c21a
Li732375 <lee732375@gmail.com>
2024-01-28 17:57:01 +0800

    learning updated

--- Cpp/constAndmuttable/account-1.h
+++ Cpp/constAndmuttable/account-1.h
@@ -15,5 +15,7 @@ class Account {
         
         void deposit(double amount);
         void withdraw(double amount);
+
+        // 雖然 to_string 並沒有變動實例狀態，也不能通過編譯。
         string to_string() const; // 如果要通過編譯的話，to_string 必須加上 const 限定。
 };
--- Cpp/constAndmuttable/account.h
+++ Cpp/constAndmuttable/account.h
@@ -16,9 +16,11 @@ class Account {
         // 直接在定義類別時撰寫。
         Account(string id, string name, double balance):
             id(id), name(name), balance(balance) {};
-        // id 值域就會用參數 id 的值初始化，name 值域就會用參數 name 的值初始化，
-        // balance 值域就會用參數 balance 的值初始化，括號中指定不一定要是參數，
-        // 也可以是運算式，如果初始式清單省略了某個值域，那就會使用預設初始化。
+        // id 值域就會用參數 id 的值初始化
+        // name 值域就會用參數 name 的值初始化，
+        // balance 值域就會用參數 balance 的值初始化，
+        // 括號中指定不一定要是參數，也可以是運算式，
+        // 如果初始式清單省略了某個值域，那就會使用預設初始化。
 
         // 建構過程，想要委由另一個版本的建構式，可以在 : 後再指定。
         Account(string id, string name) : Account(id, name, 0.0) {}
